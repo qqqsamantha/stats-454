ed788adb84ea3159917fcad17dcc37beb76363cb
qqqsamantha <xwang2@macalester.edu>
2021-12-02 22:10:29 -0600

    baseline shinyApp

--- 454_shiny_app.R
+++ 454_shiny_app.R
@@ -1,8 +1,19 @@
 library(shiny)
-library(tidyverse)
 library(rsconnect)
 library(shinythemes)
-
+library(dplyr)
+library(ggplot2)
+library(tidyverse)
+library(tidybayes)
+library(tidymodels)
+library(rstan)
+library(rstanarm)
+library(bayesplot)
+library(bayesrules)
+library(broom.mixed)
+library(pracma)
+library(probably)
+library(DT)
 
 log_hs_model_updated<-readRDS("/Users/Apple/Desktop/454_project/models/log_hs_model_updated.rds")
 
@@ -25,7 +36,7 @@ ui<-fluidPage(
                              selectInput(inputId = "userchoice3", 
                                          label = "Input Year Here", 
                                          choices= list("2015","2016","2017"),
-                                         selected=list("2015"),
+                                         selected=list("2016"),
                                          multiple = FALSE),
                              selectInput(inputId = "userchoice4", 
                                          label = "Input Month Here", 
@@ -34,11 +45,18 @@ ui<-fluidPage(
                                                        "October","November","December"),
                                          selected=list("January"),
                                          multiple = FALSE),
-                             sliderInput(inputId="userchoice5",
-                                         "arrival date week number",
-                                         min = 1,
-                                         max = 50,
-                                         value = 30),
+                             selectInput(inputId="userchoice5",
+                                         label="arrival date week number",
+                                         choices= list("1","2","3",
+                                                       "4","5","6","7","8",
+                                                       "9","10","11","12","13","14","15","16","17",
+                                                       "18","19","20","21","22","23",
+                                                       "24","25","26","27","28",
+                                                       "29","30","31","32","33","34","35","36","37",
+                                                       "38","39","40","41","42","43",
+                                                       "44","45","46","47","48","49","50","51","52","53"),
+                                         selected=list("1"),
+                                         multiple = FALSE),
                              sliderInput(inputId="userchoice6",
                                          "number of adults",
                                          min = 0,
@@ -50,7 +68,7 @@ ui<-fluidPage(
                                          max = 2,
                                          value = 1),
                              sliderInput(inputId="userchoice8",
-                                         "number of children",
+                                         "number of babies",
                                          min = 0,
                                          max = 1,
                                          value = 0),
@@ -65,14 +83,16 @@ ui<-fluidPage(
                                          choices= list("TA/TO","GDS","Corporate","Direct"),
                                          selected=list("GDS"),
                                          multiple = FALSE),
-                             selectInput(inputId = "userchoice11", 
-                                         label = "Input if repeated guest or not Here", 
-                                         choices = c(Yes = "1", No = "0"), 
-                                         multiple = FALSE),
-                             selectInput(inputId = "userchoice12", 
-                                         label = "Input if cancelled previously or not Here", 
-                                         choices = c(Yes = "1", No = "0"), 
-                                         multiple = FALSE),
+                             sliderInput(inputId="userchoice11",
+                                         "Input if repeated guest or not Here",
+                                         min = 0,
+                                         max = 1,
+                                         value = 0),
+                             sliderInput(inputId="userchoice12",
+                                         "Input if cancelled previously or not Here",
+                                         min = 0,
+                                         max = 1,
+                                         value = 0),
                              selectInput(inputId = "userchoice13", 
                                          label = "Input Assigned Room Type Here", 
                                          choices= list("A","B","C","D",
@@ -90,53 +110,38 @@ ui<-fluidPage(
                                                        "Contract","Group"),
                                          selected=list("Contract"),
                                          multiple = FALSE),
-                             selectInput(inputId = "userchoice16", 
-                                         label = "Input if required parking spaces or not Here", 
-                                         choices = c(Yes = "1", No = "0"), 
-                                         multiple = FALSE),
+                             sliderInput(inputId="userchoice16",
+                                         "Input if required parking spaces or not Here",
+                                         min = 0,
+                                         max = 1,
+                                         value = 0),
                              sliderInput(inputId="userchoice17",
                                          "total number of special requests",
                                          min = 0,
                                          max = 5,
                                          value = 1),
                              submitButton(text = "Get Predictions")),
-                mainPanel("main panel",textOutput("text1"))))
+                mainPanel("main panel",plotOutput("histgram"))))
 
 server <- function(input, output){
-  output$mapping <- renderPlot({
-    main %>%
-      group_by(county) %>%
-      mutate(total_contribs = n()) %>%
-      filter(total_contribs > 25) %>%
-      summarize(mean_amt = mean(Amount)) %>%
-      ggplot() + 
-      geom_map(map = mn_county, aes(map_id = county, fill = mean_amt)) +
-      labs(x="long",y="lat",title = "Mean amount of donations for each county") +
-      expand_limits(x = mn_county$long, y = mn_county$lat)})
-  output$timeplot <- renderPlot({
-    main %>% 
-      filter(Amount > 0) %>%
-      filter(Gender %in%  input$userchoice1, county == input$userchoice2) %>% 
-      group_by(county) %>%
-      mutate(avg = mean(Amount)) %>%
-      ungroup() %>% 
-      ggplot(aes(x = Amount, fill=county)) +
-      geom_histogram(color = "white") +
-      facet_wrap(~county, scales="free_y") +
-      geom_vline(aes(xintercept = avg),
-                 color="royalblue1", linetype="dashed", size=1) +
-      scale_x_log10(labels = scales::comma) +
-      scale_fill_brewer(palette="Blues") +
-      labs(title = "Minnesota Political Donations by County and Sex",
-           x = "",
-           y = "") +
-      theme_minimal() +
-      theme(plot.title = element_text(hjust = 0.5, face = "bold", size = 20),
-            strip.text = element_text(size=15), 
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank(),
-            panel.border = element_blank(),
-            panel.background = element_blank())})}
+  output$histgram <- renderPlot({
+    cancel_prediction<-posterior_predict(
+      log_hs_model_updated,newdata=data.frame(hotel=input$userchoice1,
+                                              arrival_date_year=input$userchoice3,
+                                              arrival_date_month=input$userchoice4,
+                                              arrival_date_week_number=input$userchoice5,
+                                              adults=input$userchoice6,children=input$userchoice7,
+                                              babies=input$userchoice8,
+                                              country=input$userchoice2,market_segment=input$userchoice9,
+                                              distribution_channel=input$userchoice10,
+                                              is_repeated_guest=input$userchoice11,
+                                              previous_cancellations=input$userchoice12,
+                                              assigned_room_type=input$userchoice13,
+                                              deposit_type=input$userchoice14,
+                                              customer_type=input$userchoice15,
+                                              required_car_parking_spaces=input$userchoice16,
+                                              total_of_special_requests=input$userchoice17))
+    mcmc_hist(cancel_prediction, prob = 0.9)})}
 
 
 
