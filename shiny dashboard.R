9511451eace0ac860a0c84c62d573475121a3d3f
yiyangshi-hub <yshi@macalester.edu>
2021-12-09 14:28:05 -0600

    a

--- shiny dashboard.R
+++ shiny dashboard.R
@@ -28,15 +28,18 @@ head <- dashboardHeader(title = "My Dashboard")
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-    menuItem("Widgets", icon = icon("th"), tabName = "widgets",
-             badgeLabel = "new", badgeColor = "green")
+    menuItem("Bayes_Logistic_Lasso", 
+             tabName = "Bayes_Logistic_Lasso", 
+             icon = icon("dashboard")),
+    menuItem("Horseshoe", 
+             tabName = "Horseshoe", 
+             icon = icon("th")))
   )
-)
+
 
 body <- dashboardBody(
   tabItems(
-    tabItem(tabName = "dashboard",
+    tabItem(tabName = "Bayes_Logistic_Lasso",
             fluidRow(
               box(
                 width = 3,height = 2,
@@ -140,8 +143,8 @@ body <- dashboardBody(
                 sliderInput(inputId="userchoice16",
                             "Input if required parking spaces or not Here",
                             min = 0,
-                            max = 1,
-                            value = 0),
+                            max = 10,
+                            value = 1),
                 sliderInput(inputId="userchoice17",
                             "Input total number of special requests",
                             min = 0,
@@ -158,8 +161,121 @@ body <- dashboardBody(
             )
     ),
     
-    tabItem(tabName = "widgets",
-            h2("Widgets tab content")
+    tabItem(tabName = "Horseshoe",
+            fluidRow(
+              box(
+                width = 3,height = 2,
+                selectInput(inputId = "userchoice1", 
+                            label = "Input Hotel type Here", 
+                            choices = list("City Hotel","Resort Hotel"), 
+                            multiple = FALSE),
+                selectInput(inputId = "userchoice2", 
+                            "Input Country Here", 
+                            choices = list("PRT","ESP","GBR","NLD","AUT","FRA","BEL",
+                                           "BRA","USA","ITA","DEU","IRL","CN","POL",
+                                           "RUS","NOR","CHN","CHE","ISR","SWE"),
+                            selected=list("PRT"),
+                            multiple = FALSE), 
+                selectInput(inputId = "userchoice3", 
+                            label = "Input Year Here", 
+                            choices= list("2015","2016","2017"),
+                            selected=list("2016"),
+                            multiple = FALSE),
+                selectInput(inputId = "userchoice4", 
+                            label = "Input Month Here", 
+                            choices= list("January","February","March","April",
+                                          "May","June","July","August","September",
+                                          "October","November","December"),
+                            selected=list("January"),
+                            multiple = FALSE),
+                selectInput(inputId="userchoice5",
+                            label="arrival date week number",
+                            choices= list("1","2","3",
+                                          "4","5","6","7","8",
+                                          "9","10","11","12","13","14","15","16","17",
+                                          "18","19","20","21","22","23",
+                                          "24","25","26","27","28",
+                                          "29","30","31","32","33","34","35","36","37",
+                                          "38","39","40","41","42","43",
+                                          "44","45","46","47","48","49","50","51","52","53"),
+                            selected=list("1"),
+                            multiple = FALSE),
+                sliderInput(inputId="userchoice6",
+                            "number of adults",
+                            min = 0,
+                            max = 4,
+                            value = 2),
+                sliderInput(inputId="userchoice7",
+                            "number of children",
+                            min = 0,
+                            max = 2,
+                            value = 1),
+                selectInput(inputId = "userchoice9", 
+                            label = "Input Market Segment Here", 
+                            choices= list("Groups","Online TA","Corporate","Direct",
+                                          "Offline TA/TO","Complementary","Aviation"),
+                            selected=list("Groups"),
+                            multiple = FALSE)),
+              
+              box(
+                width = 5, height = 5,
+                plotOutput("hs_histgram")),
+              
+              box(
+                width = 3, height = 3,
+                selectInput(inputId = "userchoice10", 
+                            label = "Input Distribution Channel Here", 
+                            choices= list("TA/TO","GDS","Corporate","Direct"),
+                            selected=list("GDS"),
+                            multiple = FALSE),
+                
+                selectInput(inputId = "userchoice11", 
+                            label = "Input if repeated guest or not Here", 
+                            choices= list("0","1"),
+                            selected=list("0"),
+                            multiple = FALSE),
+                selectInput(inputId = "userchoice12", 
+                            label = "Input if cancelled previously or not Here", 
+                            choices= list("0","1"),
+                            selected=list("0"),
+                            multiple = FALSE),
+                
+                selectInput(inputId = "userchoice13", 
+                            label = "Input Assigned Room Type Here", 
+                            choices= list("A","B","C","D",
+                                          "E","F","G","H","I","K"),
+                            selected=list("A"),
+                            multiple = FALSE),
+                selectInput(inputId = "userchoice14", 
+                            label = "Input Deposit Type Here", 
+                            choices= list("Non Refund","No Deposit","Refundable"),
+                            selected=list("Refundable"),
+                            multiple = FALSE),
+                selectInput(inputId = "userchoice15", 
+                            label = "Input Customer Type Here", 
+                            choices= list("Transient","Transient-Party",
+                                          "Contract","Group"),
+                            selected=list("Contract"),
+                            multiple = FALSE),
+                sliderInput(inputId="userchoice16",
+                            "Input if required parking spaces or not Here",
+                            min = 0,
+                            max = 10,
+                            value = 1),
+                sliderInput(inputId="userchoice17",
+                            "Input total number of special requests",
+                            min = 0,
+                            max = 5,
+                            value = 1),
+                selectInput(inputId = "userchoice18", 
+                            label = "Input Reserved Room Type Here", 
+                            choices= list("A","B","C","D",
+                                          "E","F","G","H","I","K"),
+                            selected=list("A"),
+                            multiple = FALSE),
+                submitButton(text = "Get Predictions")
+              )
+            )
     )
   )
 )
@@ -186,6 +302,33 @@ server <- function(input, output) {
                                                  required_car_parking_spaces=input$userchoice16,
                                                  total_of_special_requests=input$userchoice17))
     cancel_prediction_lasso %>% 
+      as.data.frame() %>% 
+      rename(y = `1`) %>%
+      count(y) %>% 
+      mutate(prop = n/sum(n)) %>% 
+      ggplot(aes(x = y, y = prop)) +
+      geom_col()
+  })
+  
+  output$hs_histgram <- renderPlot({
+    cancel_prediction_hs<-posterior_predict(
+      log_hs_model_updated,newdata=data.frame(hotel=input$userchoice1,
+                                              arrival_date_year=input$userchoice3,
+                                              arrival_date_month=input$userchoice4,
+                                              arrival_date_week_number=input$userchoice5,
+                                              adults=input$userchoice6,
+                                              children=input$userchoice7,
+                                              country=input$userchoice2,
+                                              market_segment=input$userchoice9,
+                                              distribution_channel=input$userchoice10,
+                                              is_repeated_guest=input$userchoice11,
+                                              previous_cancellations=input$userchoice12,
+                                              assigned_room_type=input$userchoice13,
+                                              deposit_type=input$userchoice14,
+                                              customer_type=input$userchoice15,
+                                              required_car_parking_spaces=input$userchoice16,
+                                              total_of_special_requests=input$userchoice17))
+    cancel_prediction_hs %>% 
       as.data.frame() %>% 
       rename(y = `1`) %>%
       count(y) %>% 
