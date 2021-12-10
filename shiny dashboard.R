825080c6180d0f2eb586b4fc762d049c051360d6
yiyangshi-hub <yshi@macalester.edu>
2021-12-09 18:38:28 -0600

    widgets

--- shiny dashboard.R
+++ shiny dashboard.R
@@ -21,6 +21,33 @@ log_model<-readRDS("log_model.rds")
 log_hs_model<-readRDS("log_hs_model.rds")
 log_hs_model_updated<-readRDS("log_hs_model_updated.rds")
 bayes_lasso_log_updated<-readRDS("bayes_lasso_log_updated.rds")
+hotel <- read.csv("https://raw.githubusercontent.com/yiyangshi-hub/STAT454-Project/main/hotel_bookings.csv")
+
+hotel_clean <- hotel %>% 
+  select(-reservation_status, -reservation_status_date,-company, -arrival_date_day_of_month, -days_in_waiting_list, -agent) %>% 
+  filter(country %in%
+           c("AUT","BEL","BRA","CHE","CHN","CN","DEU","ESP","FRA","GBR","IRL",
+             "ISR","ITA","NLD","NOR","POL","PRT","RUS","SWE","USA")) %>% 
+  mutate(is_canceled = factor(is_canceled),
+         hotel = factor(hotel),
+         arrival_date_year = factor(arrival_date_year),
+         arrival_date_month = factor(arrival_date_month),
+         arrival_date_week_number = factor(arrival_date_week_number),
+         meal = factor(meal),
+         market_segment = factor(market_segment),
+         distribution_channel = factor(distribution_channel),
+         is_repeated_guest = factor(is_repeated_guest),
+         reserved_room_type = factor(reserved_room_type),
+         assigned_room_type = factor(assigned_room_type),
+         deposit_type = factor(deposit_type),
+         customer_type = factor(customer_type),
+         previous_cancellations = factor(previous_cancellations),
+         country = factor(country),
+  ) %>% 
+  na.omit()
+
+set.seed(123)
+hotel_sub <- hotel_clean %>% sample_n(1000)
 
 
 
@@ -28,17 +55,111 @@ head <- dashboardHeader(title = "My Dashboard")
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
+    menuItem("Background",
+             tabName = "Background",
+             icon = icon("th")),
+    menuItem("Bayesian_Logistic_Notation",
+             tabName = "Bayesian_Logistic_Notation",
+             icon = icon("th")),
+    menuItem("Bayesian_Logistic_Lasso_Notation",
+             tabName = "Bayesian_Logistic_Lasso_Notation",
+             icon = icon("th")),
+    menuItem("Horseshoe_Notation",
+             tabName = "Horseshoe_Notation",
+             icon = icon("th")),
+    menuItem("Data_Visualization",
+             tabName = "Data_Visualization",
+             icon = icon("th")),
     menuItem("Bayes_Logistic_Lasso", 
              tabName = "Bayes_Logistic_Lasso", 
              icon = icon("dashboard")),
     menuItem("Horseshoe", 
              tabName = "Horseshoe", 
-             icon = icon("th")))
+             icon = icon("th")),
+    menuItem("Model_Evaluation", 
+             tabName = "Model_Evaluation", 
+             icon = icon("th"))
+    )
   )
 
 
 body <- dashboardBody(
   tabItems(
+    tabItem(tabName ="Background",
+            fluidRow(
+              column( width = 5,
+                box(
+                  title = "About the Data", status = "primary", solidHeader = TRUE,
+                  "This data set contains booking information for a city hotel and a resort hotel, and includes information such as when the booking was made, length of stay, the number of adults, children, and/or babies, and the number of available parking spaces, among other things."
+                ),
+                box(
+                  "Who creates the data:The data is originally from the article Hotel Booking Demand Datasets, written by Nuno Antonio, Ana Almeida, and Luis Nunes for Data in Brief, Volume 22, February 2019."
+                ),
+                box(
+                  "Data source:The data was downloaded and cleaned by Thomas Mock and Antoine Bichat for #TidyTuesday during the week of February 11th, 2020.source: https://www.kaggle.com/jessemostipak/hotel-booking-demand"
+                )
+              )
+              
+            )),
+    tabItem(tabName = "Bayesian_Logistic_Notation",
+            fluidRow(
+              box(
+                withMathJax(helpText("To model the binary response variable $$Y\\in \\{0,1\\}$$ by predictors $$X_1,X_2...X_{26}$$, suppose we collect n data points. Let $$(Y_i, X_i)$$ denotes the observed data on each case $$i\\in \\{1, 2, ..., n\\}$$. Thus the Bayesian Logistic Regression model is as follows:")),
+                withMathJax(helpText("$$Y_i | \\beta_0, \\beta_1,...,\\beta_{26}, \\sigma \\stackrel{ind}{\\sim} \\text{Bern}(\\pi_i)  \\text{where } \\log\\left(\\frac{\\pi_i}{1 - \\pi_i}\\right) = \\beta_0 + \\beta_1 X_1 + \\beta_2 X_2 +...+\\beta_{26} X_{26}$$")),
+                withMathJax(helpText("$$\\text{equivalently, } \\frac{\\pi_i}{1 - \\pi_i} = e^{\\beta_0 + \\beta_1 X_1 + \\beta_2 X_2 +...+\\beta_{26} X_{26}}$$")),
+                withMathJax(helpText("$$\\text{ and } \\pi_i = \\frac{e^{\\beta_0 + \\beta_1 X_1 + \\beta_2 X_2 +...+\\beta_{26} X_{26}}}{e^{\\beta_0 + \\beta_1 X_1 + \\beta_2 X_2 +...+\\beta_{26} X_{26}} + 1}$$")),
+                withMathJax(helpText("$$\\beta_{0c}  \\sim N(m_0, s_0^2)$$")),
+                withMathJax(helpText("$$\\beta_1    \\sim N(m_1, s_1^2)$$")),
+                withMathJax(helpText("$$\\beta_2   \\sim N(m_2, s_2^2)$$")),
+                withMathJax(helpText("$$\\text{ ... }$$")),
+                withMathJax(helpText("$$\\beta_{26}    \\sim N(m_{26}, s_{26}^2)$$"))
+              ),
+              box(
+                "Note:",
+                withMathJax(helpText("The priors on $$(\\beta_0, \\beta_1, ... \\beta_{26})$$ are independent of one another.")),
+                withMathJax(helpText("For interpretability, we state our prior understanding of the model intercept $$\\beta_0$$ through the **centered intercept** $$\\beta_{0c}$$.")),
+                withMathJax(helpText("When $$X = 0$$, $$\\beta_0$$ is the expected *logged* odds for the hotel booking is canceled and $$e^{\\beta_0}$$ is the expected *odds* for the hotel booking is canceled")),
+                withMathJax(helpText("For each 1-unit increase in $$X_1$$, $$\\beta_1$$ is the expected change in the *logged odds of the hotel booking is canceled* and $$e^{\\beta_1}$$ is the expected *multiplicative* change in *odds*.")),
+                withMathJax(helpText("$$\\beta_1 = \\log(\\text{odds}_{x_1+1}) - \\log(\\text{odds}_{x_1}) \\;\\; \\text{ and } \\;\\; e^{\\beta_1} = \\frac{\\text{odds}_{x_1+1}}{\\text{odds}_{x_1}}$$")),
+                withMathJax(helpText("Same formula will apply to the remaining predictors $$\\beta_2, ..., \\beta_{26}$$"))
+              )
+            )
+    ),
+    
+    tabItem(tabName = "Bayesian_Logistic_Lasso_Notation",
+            fluidRow(
+              box(
+                withMathJax(helpText("$$Y_i | \\beta_0, \\beta_1,...,\\beta_{26}, \\sigma  \\stackrel{ind}{\\sim} \\text{Bern}(\\pi_i)$$")),
+                withMathJax(helpText("$$\\beta_{k}  \\sim \\text{Laplace}(0, 1/\\lambda)$$")),
+                withMathJax(helpText("The priors on $$\\beta_k$$ are independent of one another. We set a laplace prior because the laplace distribution centered at 0, meaning if the coefficient shrink to 0, the corresponding variable will not be considered as one of the predictors in the model. ")),
+                withMathJax(helpText("$$\\beta_k$$ represent the coefficient of predictors $$X_{1},X_{2}...X_{26}$$ and we also assume that each of the variable follow a laplace distribution."))
+              )
+            )
+    ),
+    tabItem(tabName = "Horseshoe_Notation",
+            fluidRow(
+              box(
+                withMathJax(helpText("$$Y_i | \\beta_0, \\beta_1,...,\\beta_{26}, \\sigma  \\stackrel{ind}{\\sim} \\text{Bern}(\\pi_i)$$")),
+                withMathJax(helpText("$$\\beta_{k}|\\tau,\\lambda  \\sim \\text{N}(0, \\tau\\lambda_k)$$")),
+                withMathJax(helpText("$$\\lambda_k  \\sim \\text{HalfCauchy}(0,1)$$")),
+                withMathJax(helpText("$$\\tau  \\sim \\text{HalfCauchy}(0,\\tau_0)$$")),
+                withMathJax(helpText("The prior on $$\\beta_k$$ is a model which under the condition of $$\\lambda_k$$ and $$\\tau$$")),
+                withMathJax(helpText("The heavy-tailed Cauchy prior on $$\\lambda_k$$ allows individual coefficients to offset and global shrinkage.")),
+                withMathJax(helpText("$$\\tau$$ shares the similar heavy-tailed cauchy prior with $$\\lambda_k$$, but we need to choose $$\\tau_0$$ to determinate how fast the model shrink to 0."))
+              )
+            )
+    ),
+    tabItem(tabName = "Data_Visualization",
+            fluidRow(
+              box(
+                plotOutput("plot1", height = 250)
+              ),
+              box(
+                plotOutput("plot2", height = 250)
+              ),
+              box(
+                plotOutput("plot3", height = 250))
+            )),
     tabItem(tabName = "Bayes_Logistic_Lasso",
             fluidRow(
               box(
@@ -335,6 +456,23 @@ server <- function(input, output) {
       mutate(prop = n/sum(n)) %>% 
       ggplot(aes(x = y, y = prop)) +
       geom_col()
+  })
+  
+  output$plot1 <- renderPlot({
+    hotel_sub %>% 
+      ggplot(aes(x = hotel, fill = is_canceled)) +
+      geom_bar(position = "fill")
+  })
+  
+  output$plot2 <- renderPlot({
+    hotel_sub %>% 
+      ggplot(aes(x = previous_cancellations, fill = is_canceled)) +
+      geom_bar(position = "fill")
+  })
+  
+  output$plot3 <- renderPlot({
+    ggplot(hotel_sub, aes(x = hotel, y = is_canceled, color = previous_cancellations)) +
+      geom_jitter(height = 0.25)
   })
 }
 
--- shiny_test.R
+++ shiny_test.R
@@ -38,7 +38,7 @@ ui <- dashboardPage(
     tabItems(
       tabItem(tabName ="Background",
               fluidRow(
-                box(textOutput("About the data:This data set contains booking information for a city hotel and a resort hotel, and includes information such as when the booking was made, length of stay, the number of adults, children, and/or babies, and the number of available parking spaces, among other things.Who creates the data:The data is originally from the article Hotel Booking Demand Datasets, written by Nuno Antonio, Ana Almeida, and Luis Nunes for Data in Brief, Volume 22, February 2019.Data source:The data was downloaded and cleaned by Thomas Mock and Antoine Bichat for #TidyTuesday during the week of February 11th, 2020.source: https://www.kaggle.com/jessemostipak/hotel-booking-demand"))
+                box("About the data:This data set contains booking information for a city hotel and a resort hotel, and includes information such as when the booking was made, length of stay, the number of adults, children, and/or babies, and the number of available parking spaces, among other things.Who creates the data:The data is originally from the article Hotel Booking Demand Datasets, written by Nuno Antonio, Ana Almeida, and Luis Nunes for Data in Brief, Volume 22, February 2019.Data source:The data was downloaded and cleaned by Thomas Mock and Antoine Bichat for #TidyTuesday during the week of February 11th, 2020.source: https://www.kaggle.com/jessemostipak/hotel-booking-demand")
               )),
       tabItem(tabName ="Bayesian_Logistic",
               fluidRow(
